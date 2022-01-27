559b5af7d7148219e1cbd44d155be02a525c5401
Jaspier <dungoallen349@gmail.com>
2022-01-27 01:08:54 +0000

    Adding login and logout functionality to the frontend with redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import FaqScreen from "./screens/FaqScreen";
 import AboutScreen from "./screens/AboutScreen";
 import ContactScreen from "./screens/ContactScreen";
 import ProductScreen from "./screens/ProductScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
   return (
@@ -19,11 +20,12 @@ const App = () => {
           <Container>
             <Routes>
               <Route index element={<HomeScreen />} />
-              <Route path="/shop" element={<ShopScreen />} />
+              <Route path="/login" element={<LoginScreen />} />
               <Route path="/faq" element={<FaqScreen />} />
               <Route path="/about" element={<AboutScreen />} />
               <Route path="/contact" element={<ContactScreen />} />
               <Route path="/product/:id" element={<ProductScreen />} />
+              <Route path="/shop" element={<ShopScreen />} />
             </Routes>
           </Container>
         </main>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { logout } from "../actions/userActions";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector(state => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
     <header>
       <Navbar bg="light" expand="lg" collapseOnSelect>
@@ -16,15 +27,29 @@ const Header = () => {
               <LinkContainer to="/shop">
                 <Nav.Link>Shop</Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/faq">
-                <Nav.Link>FAQ</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/about">
-                <Nav.Link>About</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/contact">
-                <Nav.Link>Contact</Nav.Link>
-              </LinkContainer>
+
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <>
+                  <LinkContainer to="/faq">
+                    <Nav.Link>FAQ</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/about">
+                    <Nav.Link>About</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/contact">
+                    <Nav.Link>Contact</Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
