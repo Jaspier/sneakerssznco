d89cf07c08aa052f09e0b3a739242c8aedf3703f
Jaspier <dungoallen349@gmail.com>
2022-01-30 00:18:16 +0000

    Adding back products seeder

---
+++ backend/data/products.js
@@ -0,0 +1,36 @@
+const products = [
+  {
+    name: "WMNS Air Jordan 1 Mid ‘Syracuse’",
+    image: "/images/syracuse.jpg",
+    description: "Size: UK 5.5 / US 8",
+    brand: "Jordan 1s",
+    price: 180,
+    countInStock: 1,
+  },
+  {
+    name: "Air Jordan 1 Mid Light Smoke Grey/Anthracite",
+    image: "/images/anthracite.jpg",
+    description: "Size: UK 8 / US 9",
+    brand: "Jordan 1s",
+    price: 160,
+    countInStock: 1,
+  },
+  {
+    name: "WMNS Air Jordan 1 Mid Reverse Chicago",
+    image: "/images/reverse-chicago.jpg",
+    description: "Size: UK 4.5 / US 7",
+    brand: "Jordan 1s",
+    price: 180,
+    countInStock: 1,
+  },
+  {
+    name: "Nike Dunk Low Black and White ‘Panda’",
+    image: "/images/panda-dunks.jpg",
+    description: "Size: UK 5 / US 7.5",
+    brand: "Nike Dunks",
+    price: 155,
+    countInStock: 1,
+  },
+];
+
+export default products;
